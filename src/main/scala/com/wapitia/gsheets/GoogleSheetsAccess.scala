124b1e9dcd3e490310a49fced8fd86cc7b02d3a2
wapitia <wapitiasoft@gmail.com>
2018-07-24 07:01:38 -0600

    Exception handling and comments

--- src/main/scala/com/wapitia/gsheets/GoogleSheetsAccess.scala
+++ src/main/scala/com/wapitia/gsheets/GoogleSheetsAccess.scala
@@ -13,7 +13,6 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.http.javanet.NetHttpTransport
-//import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.client.util.store.FileDataStoreFactory
 import com.google.api.services.sheets.v4.Sheets
@@ -27,108 +26,170 @@ class GoogleSheetsAccess(appName: String, scopes: List[String]) {
   
   import GoogleSheetsAccess._
 
+  /** Load the values from a Google Sheet given the sheet's SID and a parsable
+   *  range string to load as a 2x2 grid of AnyRefs.
+   *  Uses default client secrets file and credentials folder.
+   *  Delivers string values as seen in the heads-up screen formats. 
+   */
   def loadSheetFormattedValues(sheetSid: String, loadRange: String) : List[List[AnyRef]] = 
-    loadSheet(sheetSid, loadRange, ValueRenderOption.FORMATTED_VALUE)
+    loadSheet(sheetSid, loadRange, ValueRenderOption.FORMATTED_VALUE, ClientSecretsFile, CredentialsFolder)
   
   /** Load the values from a Google Sheet given the sheet's SID and a parseable
    *  range string to load as a 2x2 grid of AnyRefs.
+   *  Uses default client secrets file and credentials folder.
    *  o dates as BigDecimal values which are days since 1899-12-30
    *  o numbers as BigDecimal unformatted values. This includes currency values.
    *  o other text as String values pretty much unchanged
    *  o Null object on empty cells
    */
   def loadSheetUnformattedValues(sheetSid: String, loadRange: String) : List[List[AnyRef]] = 
-    loadSheet(sheetSid, loadRange, ValueRenderOption.UNFORMATTED_VALUE)
-  
-  def loadSheetFormula(sheetSid: String, loadRange: String) : List[List[AnyRef]] = 
-    loadSheet(sheetSid, loadRange, ValueRenderOption.FORMULA)
+    loadSheet(sheetSid, loadRange, ValueRenderOption.UNFORMATTED_VALUE, ClientSecretsFile, CredentialsFolder)
   
   /** Load the values from a Google Sheet given the sheet's SID and a parseable
    *  range string to load as a 2x2 grid of AnyRefs.
-   *  The ValueRenderOption specifies the mode of the values to return:
-   *  o FORMATTED_VALUE String values given as seen on the page, such as "$1,233.98"
-   *  o UNFORMATTED_VALUE Unformatted values, BigDecimals for numbers and dates, Strings for others.
-   *  o FORMULA Formula cells are returned as Strings beginning with "=" 
+   *  Uses default client secrets file and credentials folder.
+   *  This delivers the uncalculated formulas as strings starting with "=" 
+   *  instead of the raw values where formulas are defined, but otherwise delivers data as 
+   *  `loadSheetUnformattedValues`.
    */
-	def loadSheet(sheetSid: String, loadRange: String, valueOption: ValueRenderOption) : List[List[AnyRef]] = {
-		
-	  import scala.collection.JavaConverters._
-	  import GoogleSheetsAccess._
-	  
-		try {
-			val httpTransport: NetHttpTransport  = GoogleNetHttpTransport.newTrustedTransport()
-			
-			val cred : Credential = getCredentials(httpTransport);
-			val service: Sheets = new Sheets.Builder(httpTransport, GoogleSheetsAccess.JsonFactory, cred)
-				.setApplicationName(appName)
-				.build()
-			
-			val response : ValueRange = service
-			  .spreadsheets()
-			  .values()
-			  .get(sheetSid, loadRange)
-			  .setValueRenderOption(valueOption.name)
-			  .execute()
-			  
-			response.getValues().asScala.map(_.asScala.toList).toList
-		} catch {
-		  case gse: GeneralSecurityException => throw new GSheetsSecurityException(gse)
-		  case ioe: IOException => throw new GSheetsIOException(ioe)
-		}
-	}
-	
-	/**
-	 * Creates an authorized Credential object.
-	 * 
-	 * @param HTTP_TRANSPORT
-	 *            The network HTTP Transport.
-	 * @return An authorized Credential object.
-	 * @throws IOException
-	 *             If there is no client_secret.
-	 */
-	def getCredentials(httpTransport: NetHttpTransport): Credential = {
-	  
-	  import scala.collection.JavaConverters._
+  def loadSheetFormula(sheetSid: String, loadRange: String) : List[List[AnyRef]] = 
+    loadSheet(sheetSid, loadRange, ValueRenderOption.FORMULA, ClientSecretsFile, CredentialsFolder)
+  
+  /** Load the values from a Google Sheet given the sheet's SID and a parseable
+   *  range string to load as a 2x2 grid of AnyRefs.
+   *  
+   *  @param sheetSid    is the long unique string identifying the spreadsheet on docs.google.com,
+   *                     of the form "19MQxwFoobarQId_wK6vPJmb9oZRHjqyLMNOPQRS"
+   *  @param loadRange   google-specific spreadsheet sheet, row column range specification 
+   *                     of the form "Sheet Name!A1:P35" or "Sheet Name!A:P" for all rows.
+   *  @param valueOption the value render option as expected by Google sheets, one of:                 
+   *                     o FORMATTED_VALUE String values given as seen on the page, such as "$1,233.98"
+   *                     o UNFORMATTED_VALUE Unformatted values, BigDecimals for numbers and dates, Strings for others.
+   *                     o FORMULA Formula cells are returned as Strings beginning with "="
+   *  @param clientSecretsFile Name of the pre-established client secrets file, relative to
+   *                     the resources class path and package "com/wapitia/sheets/"
+   *  @param credentialsFolder the folder where the pre-established credentials are squirreled.                                         
+   */
+  def loadSheet(
+    sheetSid: String, 
+    loadRange: String, 
+    valueOption: ValueRenderOption, 
+    clientSecretsFile: String, 
+    credentialsFolder: String) : List[List[AnyRef]] = {
+    
+    import scala.collection.JavaConverters._
+    import GoogleSheetsAccess._
+    
+    try {
+      val httpTransport: NetHttpTransport  = GoogleNetHttpTransport.newTrustedTransport()
+      
+      val cred : Credential = getCredentials(httpTransport, clientSecretsFile, credentialsFolder);
+      val service: Sheets = new Sheets.Builder(httpTransport, GoogleSheetsAccess.JsonFactory, cred)
+        .setApplicationName(appName)
+        .build()
+      
+      val response : ValueRange = service
+        .spreadsheets
+        .values
+        .get(sheetSid, loadRange)
+        .setValueRenderOption(valueOption.name)
+        .execute()
+        
+      response.getValues.asScala.map(_.asScala.toList).toList
+    } catch {
+      case gse: GeneralSecurityException => throw new GSheetsSecurityException(gse)
+      case ioe: IOException => throw new GSheetsIOException(ioe)
+    }
+  }
+  
+  /**
+   * Creates an authorized Credential object.
+   * 
+   * @param HTTP_TRANSPORT
+   *            The network HTTP Transport.
+   * @return An authorized Credential object.
+   * @throws IOException
+   *             If there is no client_secret.
+   */
+  def getCredentials(httpTransport: NetHttpTransport, clientSecretsFile: String, credentialsFolder: String): Credential = {
+    
+    import scala.collection.JavaConverters._
 
-		// Load client secrets.
-		val clientSecrets: GoogleClientSecrets = loadClientSecrets
+    // Load client secrets.
+    val clientSecrets: GoogleClientSecrets = loadClientSecrets(clientSecretsFile)
 
-		// Build flow and trigger user authorization request.
-		val fileDataStoreFactory: FileDataStoreFactory = new FileDataStoreFactory(new java.io.File(CredentialsFolder))
-		val flow: AuthorizationCodeFlow = new GoogleAuthorizationCodeFlow
-		  .Builder(httpTransport, GoogleSheetsAccess.JsonFactory, clientSecrets, scopes.asJava)
-			.setDataStoreFactory(fileDataStoreFactory)
-			.setAccessType(OfflineAccess)
-			.build()
-			
-		new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize(GoogleSheetsAccess.AuthUser)
-	}
+    // Build flow and trigger user authorization request.
+    val fileDataStoreFactory: FileDataStoreFactory = new FileDataStoreFactory(new java.io.File(credentialsFolder))
+    val flow: AuthorizationCodeFlow = new GoogleAuthorizationCodeFlow
+      .Builder(httpTransport, GoogleSheetsAccess.JsonFactory, clientSecrets, scopes.asJava)
+      .setDataStoreFactory(fileDataStoreFactory)
+      .setAccessType(OfflineAccess)
+      .build()
+      
+    new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize(GoogleSheetsAccess.AuthUser)
+  }
 
-	def loadClientSecrets: GoogleClientSecrets = {
-	  
-	  import GoogleSheetsAccess._
-	  
-		def in: InputStream = GoogleSheetsAccess.getClass.getResourceAsStream(ClientSecretsFile)
-		def inputStreamReader: InputStreamReader = new InputStreamReader(in)
-		def clientSecrets: GoogleClientSecrets = GoogleClientSecrets.load(GoogleSheetsAccess.JsonFactory, inputStreamReader)
-		clientSecrets
-	}
+  /** Load Client secrets via some JSON file generated previously from google oauth job.
+   *  The file is searched for in resources according to the protocol given by
+   *  in [[Class.getResourceAsStream(String)]] and relative to this `GoogleSheetsAccess` package,
+   *  namely "com/wapitia/gsheets/". 
+   *  Alternatively, if the file begins with a slash, "/", indicating an absolute file, 
+   *  then the file is searched for from the base of resources in the class path.
+   *  
+   *  @return the [[GoogleClientSecrets]] object from the given file in resources.
+   *   
+   *  @throws GSheetsException if the dedicated named secrets file does not exist in resources.
+   *  @throws GSheetsException if the secrets file is found, but empty.
+   *  @throws GsheetsIOException if the secrets file is unavailable or unreadable.
+   */
+  def loadClientSecrets(clientSecretsFile: String): GoogleClientSecrets = {
 
-	
+    val accessClass: Class[_ <: GoogleSheetsAccess.type] = GoogleSheetsAccess.getClass
+    
+    // for error descriptions, when necessary
+    def pkgMsg: String = packageMessage(clientSecretsFile, accessClass)
+    
+    val in: InputStream = accessClass.getResourceAsStream(clientSecretsFile)
+    if (in == null)
+      throw new GSheetsException(s"Client Secrets file is not found: $pkgMsg");
+    
+    val avail = try {
+      if (in.available() == 0) 
+        throw new GSheetsException(s"Client Secrets file is empty: $pkgMsg");
+    }
+    catch {
+      case ioe: IOException => throw new GSheetsIOException(s"Client Secrets file is unavailable: $pkgMsg", ioe);
+    }
+    val inputStreamReader: InputStreamReader = new InputStreamReader(in)
+    val clientSecrets: GoogleClientSecrets = try {
+      GoogleClientSecrets.load(GoogleSheetsAccess.JsonFactory, inputStreamReader)
+    }
+    catch {
+      case ioe: IOException => throw new GSheetsIOException(s"Client Secrets file is unreadable: $pkgMsg", ioe);
+    }
+    clientSecrets
+  }
+
+  def packageMessage(filename: String, accessClass: Class[_]) = if (filename.startsWith("/"))
+      s"""file named "$filename" in resources."""
+    else {
+      val packageName = accessClass.getPackage.getName
+      s"""file named "$filename" in resources at package "$packageName"."""
+    }
 }
 
 object GoogleSheetsAccess {
   
-	/**
-	 * Global instance of the scopes required by this quickstart. If modifying these
-	 * scopes, delete your previously saved credentials/ folder.
-	 */
-	val ReadOnlyScopes: List[String] = List(SheetsScopes.SPREADSHEETS_READONLY)
-	val ReadOnlyScope: String = SheetsScopes.SPREADSHEETS_READONLY
+  /**
+   * Global instance of the scopes required by this quickstart. If modifying these
+   * scopes, delete your previously saved credentials/ folder.
+   */
+  val ReadOnlyScopes: List[String] = List(SheetsScopes.SPREADSHEETS_READONLY)
+  val ReadOnlyScope: String = SheetsScopes.SPREADSHEETS_READONLY
   val OfflineAccess = "offline"
-	val AuthUser = "user"
-	
-	val ClientSecretsFile = "client_secret.json"
-	val JsonFactory: com.google.api.client.json.JsonFactory = JacksonFactory.getDefaultInstance()
-	val CredentialsFolder = "credentials"
+  val AuthUser = "user"
+  
+  val ClientSecretsFile = "client_secret.json"
+  val JsonFactory: com.google.api.client.json.JsonFactory = JacksonFactory.getDefaultInstance()
+  val CredentialsFolder = "credentials"
 }
--- src/main/scala/com/wapitia/gsheets/ValueRenderOption.scala
+++ src/main/scala/com/wapitia/gsheets/ValueRenderOption.scala
@@ -3,15 +3,31 @@ package gsheets
 
 import com.wapitia.common.{Enum,EValue}
 
-/** enum ValueRenderOption {
- *    FORMATTED_VALUE, UNFORMATTED_VALUE, FORMULA
- *  }
+/** Formal list of the possible value render options as 
+ *  expected in [[com.google.api.services.sheets.v4.Sheets]].
+ *  
+ *  @note the google API doesn't take an enum value as expected,
+ *        but rather a String corresponding to the name one of these 
+ *        enums.
+ *        This is probably for flexibility in derived SPIs.
+ *  
+ *  {{{ 
+ *    enum ValueRenderOption {
+ *      FORMATTED_VALUE, UNFORMATTED_VALUE, FORMULA
+ *    }
+ *  }}}
  */
 sealed trait ValueRenderOption extends ValueRenderOption.Value with EValue[ValueRenderOption]
 
 object ValueRenderOption extends Enum[ValueRenderOption] {
+  
+  /** Formatted values are the strings as seen in on the screen. */
   case object FORMATTED_VALUE extends ValueRenderOption
+  
+  /** Unformatted cell values come across as Strings and "raw" BigDecimals */
   case object UNFORMATTED_VALUE extends ValueRenderOption
+  
+  /** Delivers pre-calculated entries where formulas are given as strings starting with '=' */
   case object FORMULA extends ValueRenderOption
   
   val values = List(FORMATTED_VALUE, UNFORMATTED_VALUE, FORMULA)
