7755229ccfd05351debffa713421a7f1b1faa836
Corey Morgan <wapitiasoft@gmail.com>
2018-07-25 06:11:56 -0600

    bubbleUp recur refactoring

--- src/main/scala/com/wapitia/common/package.scala
+++ src/main/scala/com/wapitia/common/package.scala
@@ -9,7 +9,6 @@ package object common {
    *  The ordering is determined by implicit `math.Ordering[T]`
    *
    *  @param seq a sequence of any type
-   *
    */
   def canonicalList[A](seq: A*)(implicit arg0: math.Ordering[A], m: scala.reflect.ClassTag[A]): List[A] = {
     val array: Array[A] = seq.toArray
@@ -25,33 +24,34 @@ package object common {
    *  Note:
    *    This is NOT that same declaration as def stringsOf(items: AnyRef*) .
    */
-//  def stringsOf(items: AnyRef*): Seq[String] = items.map(_.toString)
   def stringsOf(items: Seq[AnyRef]): Seq[String] = items.map(_.toString)
 
   /** Bubble to the top the lowest element according to the
    *  given `comp` comparator function.
    */
-  def bubbleUp[E](rest: List[E], comp: (E,E) => Int): List[E] =
-    bubbleUp[E](rest, Nil.asInstanceOf[List[E]], comp)
+  def bubbleUp[E](list: List[E], comp: (E,E) => Int): List[E] = {
 
-  def bubbleUp[E](rest: List[E], accum: List[E], comp: (E,E) => Int): List[E] =
-  rest match {
-    // list has multiple items, compare the top two
-    case h1 :: h2 :: t  =>
-      if (comp(h1, h2) <= 0)
+    def recur(rest: List[E], accum: List[E]): List[E] =
+      rest match {
+        // list has multiple items, compare the top two
         // first <= second, so first survives and second is accumulated
-        bubbleUp(h1 :: t, h2 :: accum, comp)
-      else
+        case h1 :: h2 :: t if comp(h1, h2) <= 0 =>
+            recur(h1 :: t, h2 :: accum)
+
         // first > second, so second survives and first is accumulated
-        bubbleUp(h2 :: t, h1 :: accum, comp)
+        case h1 :: h2 :: t =>
+            recur(h2 :: t, h1 :: accum)
 
-    // list has just one element, having survived all comparisons, if any.
-    // it's returned as the top of the accumulated list
-    case h :: Nil => h :: accum
+        // list has just one element, having survived all comparisons, if any.
+        // it's returned as the top of the accumulated list
+        case h :: Nil => h :: accum
 
-    // only ever gets here if originally invoked with `List()`,
-    // since the recursive calls never send `Nil` as the first element
-    case Nil => accum
+        // only ever gets here if originally invoked with `List()`,
+        // since the recursive calls never send `Nil` as the first element
+        case Nil => accum
+      }
+
+    recur(list, Nil.asInstanceOf[List[E]])
   }
 
 }
