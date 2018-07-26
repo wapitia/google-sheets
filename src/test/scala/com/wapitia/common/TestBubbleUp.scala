f4afa535e4ad79ecb866dbb46335dbbcf4da9d19
Corey Morgan <wapitiasoft@gmail.com>
2018-07-26 06:37:24 -0600

    Bubbleup test enh

--- src/test/scala/com/wapitia/common/TestBubbleUp.scala
+++ src/test/scala/com/wapitia/common/TestBubbleUp.scala
@@ -4,39 +4,46 @@ package common
 object TestBubbleUp extends App {
 
   def testNil() {
+    println("testNil")
     val res = bubbleUp(List[Int]())
     assert(res == List())
-    println("testNil OK")
   }
 
   def testSingle() {
+    println("testSingle")
     val res = bubbleUp(List[Int](6))
     assert(res.head == 6)
-    println("testSingle OK")
   }
-  
+
   def testPresorted1() {
+    println("testPresorted1")
     val res = bubbleUp(List[Int](6, 8, 9 ,10))
     assert(res.head == 6)
-    println("testPresorted1 OK")
   }
-  
+
   def testMix1() {
+    println("testMix1")
     val res = bubbleUp(List[Int](10, 7, 4, 8))
     assert(res.head == 4)
-    println("testMix1 OK")
   }
-  
-  
+
   def testMix2() {
+    println("testMix2")
     val res = bubbleUp(List[Int](10, 8, 16, 34, 7, 4, 8))
     assert(res.head == 4)
-    println("testMix2 OK")
   }
-  
+
+  def testMix3() {
+    println("testMix3")
+    val res = bubbleUp(List[Int](34,  4, 16, 8, 7, 10, 8))
+    println(res)
+    assert(res.head == 4)
+  }
+
   testNil()
   testSingle()
   testPresorted1()
   testMix1()
   testMix2()
+  testMix3()
 }
